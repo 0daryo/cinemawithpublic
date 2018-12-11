e7117bebc3166adbd13a66dc61f4d16ef9316b85
rtrrrr <38380837+rtrrrr@users.noreply.github.com>
2018-12-11 22:09:21 +0900

    Update firebase.js

--- cinemawith/plugins/firebase.js
+++ cinemawith/plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyAjGPffQ6HLaiq_osSOIGhdbTvXFuaSIEw",
-    authDomain: "cinemawith-48653.firebaseapp.com",
-    databaseURL: "https://cinemawith-48653.firebaseio.com",
-    projectId: "cinemawith-48653",
-    storageBucket: "cinemawith-48653.appspot.com",
-    messagingSenderId: "777497580577"
+    apiKey: "pass",
+    authDomain: "pass",
+    databaseURL: "pass",
+    projectId: "pass",
+    storageBucket: "pass",
+    messagingSenderId: "pass"
   })
 }
 
